30ee1f28237fa6c36d4ff1da49360267c8941960
kitan23 <80170334+kitan23@users.noreply.github.com>
2021-04-05 17:28:24 +0700

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand">
+        <a className="navbar-brand"  href="#">
           Navbar{" "}
           <span className="badge badge-pill badge-secondary">
             {totalCounters}
